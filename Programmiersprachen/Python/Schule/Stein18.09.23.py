1e63a45f030763cdc6df43d3cfb135e1c444251c
DerDossi <manuelds@hotmail.de>
2023-09-18 08:53:20 +0200

    stein neu

---
+++ Programmiersprachen/Python/Schule/Stein18.09.23.py
@@ -0,0 +1,21 @@
+bl = 4000
+
+kv = bl * 0.146 * 0.5
+rv = bl * 0.186 * 0.5
+av = bl * 0.034 * 0.5
+pv = bl * 0.026 * 0.5
+
+if bl > 5000:
+    lohnsteuer = bl * 0.35
+elif bl > 4000:
+    lohnsteuer = bl * 0.30
+elif bl > 3000:
+    lohnsteuer = bl * 0.25
+elif bl <= 3000:
+    lohnsteuer = bl * 0.20
+
+kirchensteuer = lohnsteuer * 0.08
+
+nettolohn = bl - (kv + rv + av + pv + lohnsteuer + kirchensteuer)
+
+print(nettolohn)
